4f01780ae45ca717464526fe4073fcd855c320eb
genexus85 <arilimeelik@gmail.com>
2024-03-17 11:51:43 +0800

    Add Habit Tracker class

---
+++ src/main/java/habit/HabitTracker.java
@@ -0,0 +1,25 @@
+package habit;
+
+import exceptions.HabitException;
+
+import java.util.ArrayList;
+
+public class HabitTracker {
+    private static ArrayList<Habit> habitList = new ArrayList<>();
+
+    public HabitTracker() {
+
+    }
+
+    public void addHabit (Habit newHabit) {
+
+    }
+
+    public void listHabits() {
+
+    }
+
+    public void updateHabitCount(int habitID, String updatedCount) throws HabitException {
+
+    }
+}
