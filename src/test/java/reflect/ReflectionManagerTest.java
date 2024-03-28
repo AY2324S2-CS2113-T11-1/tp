dac6edb55b8c6bd4d988442b4ed4226f949ca18c
Damien Wee <damiwee@gmail.com>
2024-03-29 01:44:48 +0800

    Add comments to junit tests

--- src/test/java/reflect/ReflectionManagerTest.java
+++ src/test/java/reflect/ReflectionManagerTest.java
@@ -14,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ReflectionManagerTest {
 
     private ReflectionManager reflectionManager;
-
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
@@ -27,6 +26,9 @@ class ReflectionManagerTest {
     @Test
     void printFiveRandomQuestions_printAnyFiveQuestions_success() {
         reflectionManager.printFiveRandomQuestions();
+
+        // Check that exactly 5 questions, 1 header and 2 separation lines
+        // have been printed to console
         String[] lines = outputStreamCaptor.toString().trim().split("\r\n|\r|\n");
         assertFalse(outputStreamCaptor.toString().trim().isEmpty());
         assertEquals(8, lines.length);
@@ -34,6 +36,7 @@ class ReflectionManagerTest {
 
     @Test
     void unsaveReflectionQuestion_unsaveFromNonEmptyFavouritesList_success() throws ReflectException {
+        //Add question to favourites list and delete it after
         reflectionManager.printFiveRandomQuestions();
         reflectionManager.saveReflectionQuestion(1);
         assertDoesNotThrow(() -> reflectionManager.unsaveReflectionQuestion(1));
@@ -41,18 +44,23 @@ class ReflectionManagerTest {
 
     @Test
     void saveReflectionQuestion_saveFavouriteAfterGenerateQuestions_success() throws ReflectException {
+        //Save question after questions have been generated
         reflectionManager.printFiveRandomQuestions();
         assertDoesNotThrow(() -> reflectionManager.saveReflectionQuestion(1));
     }
 
     @Test
     void saveReflectionQuestion_failToSaveWhenNoQuestionsGenerated_throwReflectException() throws ReflectException {
+        //Unable to save as question has not been generated yet
         assertThrows(ReflectException.class, () -> reflectionManager.saveReflectionQuestion(1));
     }
 
     @Test
     void printHelpMenu_printHelpMenu_success() {
         reflectionManager.printHelpMenu();
+
+        // Check that exactly 5 help menu items, 1 header and 2 separation lines
+        // have been printed to console
         String[] lines = outputStreamCaptor.toString().trim().split("\r\n|\r|\n");
         assertFalse(outputStreamCaptor.toString().trim().isEmpty());
         assertEquals(8, lines.length);
@@ -60,15 +68,10 @@ class ReflectionManagerTest {
 
     @Test
     void saveReflectionQuestion_indexOutOfBounds_throwsReflectException() {
+        // Users are only allowed to input id numbers between 1 and 5
+        // as each list of newly generated questions is size 5
         assertThrows(ReflectException.class, () -> {
             reflectionManager.saveReflectionQuestion(6);
-        });
-    }
-
-    @Test
-    void unsaveReflectionQuestion_indexOutOfBounds_throwsReflectException() {
-        assertThrows(ReflectException.class, () -> {
-            reflectionManager.unsaveReflectionQuestion(6);
         });
     }
 }
