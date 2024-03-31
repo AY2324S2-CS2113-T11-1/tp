7096436d1ca6cb478d8af54e11106d48610065a8
JingHaoooo <jinghaoooo17@gmail.com>
2024-03-31 19:17:21 +0800

    Add junit tests for focus feature

---
+++ src/test/java/focus/CountupTimerTest.java
@@ -0,0 +1,43 @@
+package focus;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountupTimerTest {
+    private CountupTimer countupTimer;
+
+    @BeforeEach
+    public void setUp() {
+        countupTimer = new CountupTimer();
+    }
+
+    @Test
+    public void setStartTimer_startTimer_success() {
+        countupTimer.setStartTiming();
+        assertEquals(true,countupTimer.getStartedStatus());
+    }
+
+    @Test
+    public void setStopTimer_stopTimer_success() {
+        countupTimer.setStartTiming();
+        countupTimer.setStopTiming();
+        assertEquals(false, countupTimer.getStartedStatus());
+    }
+
+    @Test
+    public void setPause_pauseTimer_success() {
+        countupTimer.setStartTiming();
+        countupTimer.setPause();
+        assertEquals(true, countupTimer.getPauseStatus());
+    }
+
+    @Test
+    public void setResume_resumeTimer_success() {
+        countupTimer.setStartTiming();
+        countupTimer.setPause();
+        countupTimer.setResume();
+        assertEquals(false, countupTimer.getPauseStatus());
+    }
+}
