ac7621eeaddff60ba2e8aa70c250b9c854dde2cf
Daviancold <110613918+Daviancold@users.noreply.github.com>
2024-04-02 18:04:49 +0800

    Fix bug for delete and help command in Fitness Motivator

--- src/main/java/commands/fitnesscommands/HelpExerciseCommand.java
+++ src/main/java/commands/fitnesscommands/HelpExerciseCommand.java
@@ -4,12 +4,22 @@ import commands.Command;
 import exceptions.FitnessException;
 import fitness.FitnessMotivator;
 
+import static commands.fitnesscommands.ErrorMessageConstants.ADDITIONAL_PARAMS_ERROR_MESSAGE;
+
 public class HelpExerciseCommand implements Command {
 
     private FitnessMotivator fitnessMotivator;
 
-    public HelpExerciseCommand (FitnessMotivator fitnessMotivator) {
+    public HelpExerciseCommand (FitnessMotivator fitnessMotivator, String commandArgs)
+            throws FitnessException {
         this.fitnessMotivator = fitnessMotivator;
+        checkCommandArgs(commandArgs);
+    }
+
+    private void checkCommandArgs(String commandArgs) throws FitnessException {
+        if (!commandArgs.isBlank()) {
+            throw new FitnessException(ADDITIONAL_PARAMS_ERROR_MESSAGE);
+        }
     }
 
     @Override
